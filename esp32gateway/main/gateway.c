930cb3fde8dc08c3b3daa52f225bf0e44cba71a2
Wally <wschageman@proton.me>
2025-12-07 21:34:56 -0700

    fartpoop

--- esp32gateway/main/gateway.c
+++ esp32gateway/main/gateway.c
@@ -31,7 +31,7 @@ static const uart_port_t uart_num = UART_NUM_2;
 
 void init_uart() {
   uart_config_t uart_config = {
-      .baud_rate = 921600,
+      .baud_rate = 115200,
       .data_bits = UART_DATA_8_BITS,
       .parity = UART_PARITY_DISABLE,
       .stop_bits = UART_STOP_BITS_1,
